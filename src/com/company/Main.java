8f38000077c2e8c1063f1f2ee13c32326b2ae73c
Orsan <orsann@gmai.com>
2022-05-18 19:24:47 -0700

    GitProject1

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // write your code here
+//        teamlead project
+        System.out.println("I am Groot!!!");
+        System.out.println("I am Groot2!!!");
+        System.out.println("I am Groot3!!!");
+
+        System.out.println("i am correct code");
+        System.out.println("i am not confused!");
+        System.out.println("helooo my friend");
+
+
+        System.out.println("hello i am your group member");
+        System.out.println("hey team lead ksajhfkjahfjkahfjhakfjhsn" +
+                "sakfjksajflksajdkfldsja" +
+                "alksfjlkdsajfkjdshfkjds");
+
+    }
+}
